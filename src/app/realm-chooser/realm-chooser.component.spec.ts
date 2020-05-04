625ef457fec93c43ea72500ba1a61b756a56d58a
Furnell <will.furnell@stfc.ac.uk>
2020-05-04 15:04:02 +0100

    Add provider for RealmService in realmchooser

--- src/app/realm-chooser/realm-chooser.component.spec.ts
+++ src/app/realm-chooser/realm-chooser.component.spec.ts
@@ -1,14 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RealmChooserComponent } from './realm-chooser.component';
+import { RealmService } from '../services/realm.service';
+import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 describe('RealmChooserComponent', () => {
   let component: RealmChooserComponent;
   let fixture: ComponentFixture<RealmChooserComponent>;
+  let realmService;
 
   beforeEach(async(() => {
+    realmService = jasmine.createSpyObj(['getRealms']);
+    realmService.getRealms.and.returnValue(of(
+      {
+        totalResults: 5,
+        itemsPerPage: 50,
+        startIndex: 0,
+        resources: [
+          {
+            name: 'alice'
+          },
+          {
+            name: 'atlas'
+          },
+          {
+            name: 'cms'
+          },
+          {
+            name: 'iam'
+          },
+          {
+            name: 'lhcb'
+          }
+        ]
+      }
+    ));
+
     TestBed.configureTestingModule({
-      declarations: [ RealmChooserComponent ]
+      declarations: [ RealmChooserComponent ],
+      providers: [
+        { provide: RealmService, useValue: realmService }
+      ]
     })
     .compileComponents();
   }));
